669728e5f6bb70ed201415d81ebdfa431464114c
MoSalem149 <mohamedwaelsalem149@gmail.com>
2024-11-18 04:13:19 -0800

    excute 0-starwars_characters commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,31 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 
-const endpoint = "https://swapi-api.hbtn.io/api";
+const endpoint = 'https://swapi-api.hbtn.io/api';
 const filmId = process.argv[2];
 
-request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
-  if (error) return console.log(error);
+request(`${endpoint}/films/${filmId}/`, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
 
   const characters = JSON.parse(body).characters;
 
-  for (const character of characters) {
-    await new Promise((resolve, reject) => {
-      request(character, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          console.log(JSON.parse(body).name);
-          resolve(body);
-        }
-      });
-    });
-  }
+  // Use Promise.all to fetch all character names in parallel
+  Promise.all(
+    characters.map(
+      (character) =>
+        new Promise((resolve, reject) => {
+          request(character, (error, response, body) => {
+            if (error) reject(error);
+            else resolve(JSON.parse(body).name);
+          });
+        })
+    )
+  )
+    .then((names) => {
+      names.forEach((name) => console.log(name));
+    })
+    .catch((error) => console.log(error));
 });
